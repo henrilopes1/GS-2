0fdfa987b3fef60b8445a57e27f1d44b76b5c3d0
Henri Lopes <82175620+henrilopes1@users.noreply.github.com>
2023-11-23 14:13:22 -0300

    Update gs.py

--- gs.py
+++ gs.py
@@ -1,4 +1,85 @@
-# ... (código anterior)
+from time import sleep
+import json
+import os
+
+# Função para calcular o tamanho da barra de progresso
+def tamProg(titulo='MindWell - Monitoramento de Saúde Mental'):
+    return len(titulo) * 3
+
+# Função para imprimir uma linha simples
+def linSimples(tam):
+    print('-' * tam)
+
+# Função para imprimir uma linha decorativa
+def linDetalhe(tam):
+    print('-=' * tam)
+
+# Função para imprimir uma mensagem com sublinhado
+def subli(frase, variavel):
+    linSimples(len(frase + str(variavel)))
+    sleep(1)
+
+# Função para mostrar menu de voltar ou sair
+def menuSairVoltar():
+    sleep(0.4)
+    print()
+    print(' ' * 5, '~' * 46)
+    print(' ' * 10, f'{"[888] Voltar ao Menu Principal":^{35}}')
+    print(' ' * 10, f'{"[999] Sair do aplicativo":^{35}}')
+    print(' ' * 5, '~' * 46)
+    print()
+
+# Função para imprimir mensagem de voltar ao menu
+def voltandoMenu(menu):
+    sleep(0.7)
+    print('\n')
+    linSimples(len(f'Voltando ao Menu {menu}...'))
+    print(f'Voltando ao Menu {menu}...')
+    linSimples(len(f'Voltando ao Menu {menu}...'))
+    print()
+    sleep(1.3)
+
+# Função para imprimir mensagem de carregamento do menu
+def carregandoMenu(menu):
+    print('\n')
+    linSimples(len(f'Carregando o Menu {menu}...'))
+    print(f'Carregando o Menu {menu}...')
+    linSimples(len(f'Carregando o Menu {menu}...'))
+    print()
+    sleep(0.5)
+
+# Função para tratar erros de entrada
+def tratarErro(inputTratamento='', tipo=''):
+    try:
+        if tipo == 'str':
+            if not isinstance(inputTratamento, str):
+                raise ValueError
+        elif tipo == 'int':
+            if not inputTratamento.isdigit():
+                raise ValueError
+            else:
+                return int(inputTratamento)
+        else:
+            raise ValueError
+
+    except Exception:
+        print('\n')
+        print('\033[31m', f"{'#~~~~~~~~~~~~~~~~~~~# ERRO DE INPUT #~~~~~~~~~~~~~~~~~~~#':^{tamProg()}}")
+        print(f"{f'Opção {inputTratamento} inválida. Por favor, tente novamente':^{tamProg()}}", '\033[m')
+        sleep(0.7)
+        return inputTratamento
+
+# Função para mostrar dicas personalizadas com base na emoção selecionada
+def mostrarDicasPersonalizadas(emocional):
+    print()
+    print(f"{emocional.capitalize()}:\n")
+
+    # Lógica para mostrar dicas com base na emoção selecionada
+    if emocional == "1":
+        print("Mantenha-se ativo fisicamente, incorporando exercícios regulares à sua rotina.")
+        print("Estabeleça metas realistas e celebre suas conquistas, por menores que sejam.")
+        print("Mantenha uma dieta equilibrada para sustentar níveis de energia consistentes.")
+    # Adicione lógica semelhante para as outras emoções
 
 # Função para mostrar dicas de acordo com o problema escolhido
 def mostrarDicasProblema(problema):
@@ -10,10 +91,38 @@ def mostrarDicasProblema(problema):
         print("Converse com alguém de confiança sobre seus sentimentos.")
         print("Considere procurar a ajuda de um profissional, como um psicólogo.")
         print("Pratique técnicas de relaxamento, como meditação ou respiração profunda.")
-
     # Adicione lógica semelhante para os outros problemas
 
-# ... (código anterior)
+# Verificar se o arquivo JSON de dados existe
+if not os.path.exists("dados_mindwell.json"):
+    with open("dados_mindwell.json", "w") as dados_mindwell_json:
+        json.dump({}, dados_mindwell_json)
+
+# Carregar dados do arquivo JSON
+with open("dados_mindwell.json", "r") as dados_mindwell_json:
+    dados_mindwell = json.load(dados_mindwell_json)
+
+# Verificar se o arquivo JSON de sugestões existe
+if not os.path.exists("sugestoes.json"):
+    with open("sugestoes.json", "w") as sugestoes_json:
+        json.dump([], sugestoes_json)
+
+# Perguntar se a pessoa deseja fazer o cadastro
+cadastro = input('\nVocê é um novo usuário? (Digite "sim" ou "não"): ')
+if cadastro.lower() == 'sim':
+    nome = input('Digite seu nome: ')
+    emocional = input('\nComo você está se sentindo hoje?\n1. Disposto\n2. Bem\n3. Cansado\n4. Ansioso(a)\n5. Triste\n6. Irritado(a)\n\nEscolha o número correspondente: ')
+    mostrarDicasPersonalizadas(emocional)
+
+    # Salvar os dados no arquivo JSON
+    dados_mindwell[nome] = {
+        "Emocional": emocional,
+        "UltimaAtividade": "",
+        "SugestaoPersonalizada": ""
+    }
+
+    with open("dados_mindwell.json", "w") as dados_mindwell_json:
+        json.dump(dados_mindwell, dados_mindwell_json)
 
 # Loop principal
 ligado = True
@@ -77,7 +186,6 @@ while ligado:
         salvarSugestao(titulo_sugestao, mensagem_sugestao)
 
         print("Sugestão enviada com sucesso!")
-        mostrarSugestoes()
         voltandoMenu('Principal')
 
     elif resp == 4:  # Menu dicas personalizadas
